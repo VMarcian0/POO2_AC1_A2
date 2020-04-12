4ad31fea3dff2804a5b7d600cdcc11fe3f85a946
VMarcian0 <victor_marcian0@hotmail.com>
2020-04-12 16:56:49 -0300

    Leitura Funcionando porém insert com problema

---
+++ src/main/java/com/example/ac1_a2/controle/FuncionarioController.java
@@ -0,0 +1,54 @@
+package com.example.ac1_a2.controle;
+
+
+import com.example.ac1_a2.entidade.Funcionario;
+import com.example.ac1_a2.services.FuncionarioServices;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * FuncionarioController
+ */
+
+@Controller 
+public class FuncionarioController {
+
+    @Autowired
+    private FuncionarioServices service;
+
+    //retorna todos os funcionarios cadastrados
+    @GetMapping("/funcionarios")
+    public ModelAndView getFuncionarios()
+    {
+        ModelAndView mv = new ModelAndView("funcionarios");
+        mv.addObject("Funcionarios", service.getFuncionarios());
+        return mv;
+    }
+
+    //tenta adicionar um funcionario e atualiza a lista de funcionarios cadastrados 
+    @PostMapping("/funcionarios")
+    public ModelAndView funcionarioAddForm(@ModelAttribute Funcionario Funcionario)
+    {
+        ModelAndView mv = new ModelAndView("funcionarios");
+        mv.addObject("Funcionario", Funcionario);
+        try
+        {
+            service.insertFuncionario(Funcionario);
+        }
+        catch(Exception e)
+        {
+           return mv; 
+        }
+        finally
+        {
+            mv.addObject("Funcionarios", service.getFuncionarios());
+        }
+        return mv;
+    }
+
+}
